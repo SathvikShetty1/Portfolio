d4f71b475ec96e53adad7ce18d7cbcd45d9f123d
Sathvik Shetty <sathvikshetty794@gmail.com>
2025-07-13 11:03:35 +0000

    the gap is still there

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -80,7 +80,7 @@ export default function Home() {
       {/* Hero Section */}
       <section id="home" className="py-[10px] md:py-16">
         <div className="container mx-auto text-center">
-            <div className="flex justify-center mb-[-100px] h-[500px] md:h-[600px] items-center">
+            <div className="flex justify-center mb-[-100px] h-[400px] md:h-[600px] items-center">
               {mounted ? (
                   <Image 
                     src={imageSrc}
